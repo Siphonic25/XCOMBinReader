c11468c7a4c2ca39352496bf3804649fd446d0b5
Siphonic25 <laparr83@gmail.com>
2024-09-15 13:07:20 +0100

    Set out plans

--- BinReader/Program.cs
+++ BinReader/Program.cs
@@ -1,10 +1,14 @@
-﻿using System;
-
-namespace BinReader
+﻿namespace BinReader
 {
     internal class Program
     {
-        //short-term goal
+        /*list of goals:
+         * Step 1: Be able to read and reoutput a .bin file
+         * Step 2: Output personal information into a nice table format
+         * Step 3: Output this information into a .txt file
+         * Step 4: exeify this
+         * (the goal is an .exe where you run it in a folder with .bin files, and it returns .txtified versions of all of them)
+         */
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
