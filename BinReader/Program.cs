ab648ff2b9f94178aa97e831fee42d3786421c9c
Siphonic25 <laparr83@gmail.com>
2024-09-15 12:41:33 +0100

    Started project.

--- BinReader/Program.cs
+++ BinReader/Program.cs
@@ -4,6 +4,7 @@ namespace BinReader
 {
     internal class Program
     {
+        //short-term goal
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
