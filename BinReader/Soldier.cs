7b7c8cec2d95ca41d07b62162f99883997446dbd
Siphonic25 <laparr83@gmail.com>
2024-09-16 23:08:45 +0100

    Created skeleton of Soldier class.

---
+++ BinReader/Soldier.cs
@@ -0,0 +1,17 @@
+﻿namespace BinReader
+{
+    public class Soldier
+    {
+        private string firstName;
+        private string lastName;
+        private string nickName;
+        private string soldierClass;
+        private string gender;
+        private string nationality;
+        private string[] bio;
+
+        public Soldier()
+        {
+        }
+    }
+}
